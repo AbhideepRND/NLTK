37c59fb27613c2af36de40a263c0ed8199fed9b2
AbhideepRND <samtel09@gmail.com>
2018-06-05 02:41:56 +0530

    Final Just Dial scraper

--- NLTKExp/sanjay/JustDial.py
+++ NLTKExp/sanjay/JustDial.py
@@ -20,7 +20,7 @@ class JustDial(object):
             url_comp = self.url + '/' + city+ '/' + board
             self.CityName = city
             self.populateSchoolUrls(url_comp)
-           # self.extractSchoolDetails()
+            self.extractSchoolDetails()
             self.file.close();
 
     def populateSchoolUrls(self, url_comp):
@@ -36,26 +36,78 @@ class JustDial(object):
         if self.nextURL != None:
             self.populateSchoolUrls(self.nextURL);
 
-    #School Name#~#Address#~#Contact Person#~#MobileNo#~#Email#~#website#~#Services#~#Link
-    def extractSchoolDetails(self, url):
-       # for url in self.urlMap:
+    def extractSchoolDetails(self):
+
+        for url in self.urlMap:
+            finalList = []
+            print(url)
+            req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            page = urlopen(req).read().decode('utf8', 'ignore')
+            soup = BeautifulSoup(page, 'lxml')
+            finalList.append(soup.find('div',{'class','company-details'}).find('span',{'class','fn'}).text)  #School Name
+            finalList.append(''.join(soup.find('span',{'id','adrstxtr'}).text.splitlines()))                #Address
+            finalList.append('')
+            if soup.find('span', {'class', 'telCntct'}) != None:
+                if len(soup.find('span', {'class', 'telCntct'}).find('a').text) < 2:
+                    finalList.append(self.extractPhoneNo(soup))
+                else:
+                    soup.find('span', {'class', 'telCntct'})
+                    finalList.append(
+                        ';'.join(v[-10:] for v in soup.find('span', {'class', 'telCntct'}).text.strip().split(',')))
+            else:
+                finalList.append('')
+            finalList.append('')                                                                            #Email
+            tmp = soup.find('i', {'class', 'web_ic'})
+            finalList.append(tmp.parent.text.strip() if (tmp != None and tmp.parent.name == 'li') else '')  #website
+            finalList.append(','.join(v.replace('\t','') for v in soup.find('span', {'class', 'showmore'}).text.strip().splitlines())) #Services
+            finalList.append(url)
+            val = '#~#'.join(str(v) for v in finalList)
+            self.file.write(val + '\n')
+
+    def extractPhoneNo(self, data):
+        tmp2 = data.findAll('style')
+        style = tmp2[1].text
+        finalPhone=[]
+        phoneNoList = data.find('span',{'class','telCntct'}).find('a').contents
+        for span in phoneNoList[4:]:
+            if span != ' ':
+                label = span.get('class')[1]
+                index =style.find(label)+label.__len__()+21
+                finalPhone.append(int(style[index:index+2])-1)
+        return ''.join(str(v) for v in finalPhone)
+
+'''
+    def extractSchoolDetails(self,url):
+        finalList = []
+        print(url)
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         page = urlopen(req).read().decode('utf8', 'ignore')
         soup = BeautifulSoup(page, 'lxml')
-        print(soup.find('div',{'class','company-details'}).find('span',{'class','fn'}).text)
-        print(soup.find('span',{'class','telCntct'}).text)
-        print(soup.find('span',{'id','adrstxtr'}).text)
-        print(soup.find('span',{'class','showmore'}).text)
-        tmp= soup.find('i',{'class','web_ic'})
-        print(tmp.parent.text if (tmp != None and tmp.parent.name == 'li') else '')
-
-
-
+        finalList.append(soup.find('div',{'class','company-details'}).find('span',{'class','fn'}).text)  #School Name
+        finalList.append(''.join(soup.find('span',{'id','adrstxtr'}).text.splitlines()))                #Address
+        finalList.append('')                                                                            #Contact Person
+        if len(soup.find('span', {'class', 'telCntct'}).find('a').text)<2:
+            finalList.append(self.extractPhoneNo(soup))
+        else:
+            soup.find('span', {'class', 'telCntct'})
+            finalList.append(';'.join(v[-10:] for v in soup.find('span', {'class', 'telCntct'}).text.strip().split(',')))
+        finalList.append('')                                                                            #Email
+        tmp = soup.find('i', {'class', 'web_ic'})
+        finalList.append(tmp.parent.text.strip() if (tmp != None and tmp.parent.name == 'li') else '')  #website
+        finalList.append(','.join(v.replace('\t','') for v in soup.find('span', {'class', 'showmore'}).text.strip().splitlines())) #Services
+        finalList.append(url)
+        val = '#~#'.join(str(v) for v in finalList)
+        print(val)
+'''
 
 board ="Pre-Schools"
 
 city=['Kolkata']
 
-sulekha = JustDial()
-#sulekha.parseCityData(board,city)
-sulekha.extractSchoolDetails('https://www.justdial.com/Kolkata/Mount-Litera-Zee-School-DHR-Near-Bishnupu-Joka/033PXX33-XX33-180409171230-J7I3_BZDET?xid=S29sa2F0YSBQcmUgU2Nob29scw==')
+justDial = JustDial()
+justDial.parseCityData(board,city)
+#justDial.extractSchoolDetails('https://www.justdial.com/Kolkata/Blue-Butterfly-Pre-School-Near-Old-St-Xaviers-Building-Sodepur/033PXX33-XX33-170919152844-Q7A1_BZDET?xid=S29sa2F0YSBQcmUgU2Nob29scw==')
+
+#https://www.justdial.com/Kolkata/Kidzee-Habra/9999PXX33-XX33-170113124959-Y7C3_BZDET?xid=S29sa2F0YSBQcmUgU2Nob29scw==
+#https://www.justdial.com/Kolkata/Blue-Butterfly-Pre-School-Near-Old-St-Xaviers-Building-Sodepur/033PXX33-XX33-170919152844-Q7A1_BZDET?xid=S29sa2F0YSBQcmUgU2Nob29scw==
+#https://www.justdial.com/Kolkata/Aptech-Montana-International-Preschool-Behind-Coal-Bhawan-New-Town/033PXX33-XX33-180326150150-F4M7_BZDET?xid=S29sa2F0YSBQcmUgU2Nob29scw==
